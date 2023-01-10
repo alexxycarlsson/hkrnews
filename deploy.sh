bf35e22bc095bca9c3d35d5028ab2c5ac7e4b452
alexxycarlsson <filipkarlsson1@gmail.com>
2023-01-10 15:11:48 +0100

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [ -d "dist" ]; then
+    rm -rf dist
+fi
+
+yarn build
+
+cd dist
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:alexxycarlsson/hkrnews.git main:gh-pages
+
+
+cd -
