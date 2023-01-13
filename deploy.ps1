7f4a8ceb97d76cc0a872921b7bf60589c8ed4757
Alexander Carlsson <filipkarlsson1@gmail.com>
2023-01-13 21:28:10 +0100

    Changed deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -9,8 +9,8 @@ Set-Location "dist"
 git init
 git checkout -B main
 git add -A
-git commit -m 'deploy'
-git push -f git@github.com:alexxycarlsson/hkrnews.git main:gh-pages
+git commit -m "deploy"
+git push -f "git@github.com:alexxycarlsson/hkrnews.git main:gh-pages"
 
 Set-Location ".."
 
